ccd4531cebbaf10aefc59d4c64a0aa23003af4b1
lzy1960 <40552111+lzy1960@users.noreply.github.com>
2023-03-28 09:57:20 +0000

    Deploying to gh-pages from @ lzy1960/lzy1960.github.io@08fc5fb2b3dfc1e8b2ffebde97f01e262f26eea7 🚀

---
+++ scripts/video.ts
@@ -0,0 +1,16 @@
+function initVideo () {
+  const div = document.createElement('div')
+  div.setAttribute('id', 'hero-video-wrapper')
+
+  const video = document.createElement('video')
+  video.setAttribute('id', 'hero-video')
+  video.src = '/background.mp4'
+  video.autoplay = true
+  video.loop = true
+  video.muted = true
+
+  const blogHero = document.querySelector('.blog-hero')
+  div.appendChild(video)
+  blogHero?.appendChild(div)
+}
+initVideo()
