fbdcc57f99e8b8fed9374b04dd30d49688da316b
lzy1960 <1960553899@qq.com>
2023-03-30 22:17:44 +0800

    chore: delete useless file

--- src/.vuepress/theme/config.ts
+++
@@ -1,9 +0,0 @@
-// .vuepress/theme/config.ts
-import { defineClientConfig } from "@vuepress/client";
-import BlogHome from "./layouts/BlogHome.vue";
-
-export default defineClientConfig({
-  layouts: {
-    BlogHome,
-  },
-});
