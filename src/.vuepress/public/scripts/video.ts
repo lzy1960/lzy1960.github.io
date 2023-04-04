c444818f8948e7fefa5be6fcdd676179ac89b029
lzy1960 <1960553899@qq.com>
2023-04-04 09:11:20 +0800

    chore: add video poster

--- src/.vuepress/public/scripts/video.ts
+++ src/.vuepress/public/scripts/video.ts
@@ -4,6 +4,7 @@ function initVideo () {
 
   const video = document.createElement('video')
   video.setAttribute('id', 'hero-video')
+  video.poster = '/background.jpg'
   video.src = '/background.mp4'
   video.autoplay = true
   video.loop = true
