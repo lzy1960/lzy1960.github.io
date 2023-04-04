0f475368d0cad421b56e5340f56109cce6cccb6e
lzy1960 <40552111+lzy1960@users.noreply.github.com>
2023-04-04 15:16:25 +0000

    Deploying to gh-pages from @ lzy1960/lzy1960.github.io@c444818f8948e7fefa5be6fcdd676179ac89b029 🚀

---
+++ assets/errorDiagram-bb949655-77ba559f.js
@@ -0,0 +1,1 @@
+import{aC as r}from"./mermaid.core-9dab87a2.js";import"./app-77de7412.js";import"./framework-b947aea2.js";const e=()=>"",t=e,p={db:{clear:()=>{}},styles:t,renderer:r,parser:{parser:{yy:{}},parse:()=>{}},init:()=>{}};export{p as diagram};
--- scripts/video.js
+++ scripts/video.js
@@ -3,7 +3,6 @@ function initVideo() {
     div.setAttribute('id', 'hero-video-wrapper');
     var video = document.createElement('video');
     video.setAttribute('id', 'hero-video');
-    video.poster = '/background.jpg';
     video.src = '/background.mp4';
     video.autoplay = true;
     video.loop = true;
